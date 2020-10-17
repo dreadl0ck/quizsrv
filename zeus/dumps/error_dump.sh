1c5cd186c991091027c4ac61cb07615cea1085fe
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-10-18 00:15:24 +0200

    autplay styling

--- zeus/dumps/error_dump.sh
+++ zeus/dumps/error_dump.sh
@@ -1,105 +1,14 @@
 #!/bin/bash
 #
 # ZEUS Error Dump
-# Timestamp: [Sun Oct 18 00:08:25 2020]
+# Timestamp: [Sun Oct 18 00:15:15 2020]
 # Error: exit status 1
 # StdErr: 
-# recv: answer
-# read: websocket: close 1001 (going away)
-# recv: answer
 # read: websocket: close 1001 (going away)
 # read: websocket: close 1001 (going away)
-# recv: answer
-# recv: next
 # read: websocket: close 1001 (going away)
 # read: websocket: close 1001 (going away)
-# recv: answer
-# recv: next
 # read: websocket: close 1001 (going away)
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# read: websocket: close 1001 (going away)
-# recv: answer
-# recv: next
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# recv: answer
-# recv: next
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# recv: answer
-# read: websocket: close 1001 (going away)
-# recv: answer
-# read: websocket: close 1001 (going away)
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# read: websocket: close 1001 (going away)
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# read: websocket: close 1001 (going away)
-# recv: answer
-# read: websocket: close 1001 (going away)
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# read: websocket: close 1001 (going away)
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# read: websocket: close 1001 (going away)
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
 # recv: answer
 # read: websocket: close 1001 (going away)
 # read: websocket: close 1001 (going away)
@@ -107,12 +16,7 @@
 # recv: answer
 # recv: next
 # recv: answer
-# recv: next
-# recv: answer
-# recv: next
 # read: websocket: close 1001 (going away)
-# recv: answer
-# recv: next
 # signal: interrupt
 # 
 
