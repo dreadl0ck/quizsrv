13d648a88b1086913af293327f130270521e9184
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-10-18 00:29:05 +0200

    improve play button scaling

--- zeus/dumps/error_dump.sh
+++ zeus/dumps/error_dump.sh
@@ -1,28 +1,78 @@
 #!/bin/bash
 #
 # ZEUS Error Dump
-# Timestamp: [Sun Oct 18 00:15:15 2020]
+# Timestamp: [Sun Oct 18 00:28:56 2020]
 # Error: exit status 1
 # StdErr: 
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# recv: answer
 # read: websocket: close 1001 (going away)
 # read: websocket: close 1001 (going away)
 # read: websocket: close 1001 (going away)
 # recv: answer
 # recv: next
 # recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# read: websocket: close 1001 (going away)
+# read: websocket: close 1001 (going away)
+# recv: answer
+# read: websocket: close 1001 (going away)
+# read: websocket: close 1001 (going away)
+# read: websocket: close 1001 (going away)
+# read: websocket: close 1001 (going away)
+# read: websocket: close 1001 (going away)
+# read: websocket: close 1001 (going away)
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# recv: next
+# recv: answer
+# read: websocket: close 1001 (going away)
+# recv: answer
+# recv: next
+# read: websocket: close 1001 (going away)
 # read: websocket: close 1001 (going away)
 # signal: interrupt
 # 
 
 
 #!/bin/bash
-VERSION="v0.2"
+VERSION="v0.2.1"
 
 
 
