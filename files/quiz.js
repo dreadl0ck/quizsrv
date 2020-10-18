ce56435a41e419de2d92f871e3e6bd4ccc8d9f6c
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-10-18 02:14:29 +0200

    nosleep

--- files/quiz.js
+++ files/quiz.js
@@ -141,6 +141,17 @@ window.addEventListener("load", function (evt) {
         }
     }
 
+    var noSleep = new NoSleep();
+
+    function enableNoSleep() {
+        noSleep.enable();
+        document.removeEventListener('touchstart', enableNoSleep, false);
+    }
+
+    // Enable wake lock.
+    // (must be wrapped in a user input event handler e.g. a mouse or touch handler)
+    document.addEventListener('touchstart', enableNoSleep, false);
+
     document.getElementById('fullscreen').onclick = openFullscreen;
 
     function checkKey(e) {
--- zeus/dumps/error_dump.sh
+++ zeus/dumps/error_dump.sh
@@ -1,65 +1,11 @@
 #!/bin/bash
 #
 # ZEUS Error Dump
-# Timestamp: [Sun Oct 18 01:59:10 2020]
+# Timestamp: [Sun Oct 18 02:14:18 2020]
 # Error: exit status 1
 # StdErr: 
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# recv: answer
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# recv: answer
-# read: websocket: close 1001 (going away)
-# recv: answer
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
-# read: websocket: close 1001 (going away)
 # recv: answer
 # recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# read: websocket: close 1006 (abnormal closure): unexpected EOF
-# read: websocket: close 1006 (abnormal closure): unexpected EOF
-# read: websocket: close 1006 (abnormal closure): unexpected EOF
-# read: websocket: close 1006 (abnormal closure): unexpected EOF
-# read: websocket: close 1006 (abnormal closure): unexpected EOF
-# read: websocket: close 1006 (abnormal closure): unexpected EOF
-# read: websocket: close 1006 (abnormal closure): unexpected EOF
-# read: websocket: close 1006 (abnormal closure): unexpected EOF
-# recv: answer
-# recv: next
-# read: websocket: close 1006 (abnormal closure): unexpected EOF
-# recv: answer
-# recv: next
-# recv: answer
-# recv: next
-# recv: answer
-# read: websocket: close 1006 (abnormal closure): unexpected EOF
-# recv: answer
 # signal: interrupt
 # 
 
