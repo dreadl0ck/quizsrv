64681a206c7739211f9c7db9fc192731e3aaefc5
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-10-20 12:39:57 +0200

    fixes and more arch questions

--- zeus/dumps/error_dump.sh
+++ zeus/dumps/error_dump.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
 # ZEUS Error Dump
-# Timestamp: [Mon Oct 19 23:25:31 2020]
-# Error: exit status 255
+# Timestamp: [Tue Oct 20 00:51:10 2020]
+# Error: exit status 1
 # StdErr: 
+# Head https://production.cloudflare.docker.com/registry-v2/docker/registry/v2/blobs/sha256/ca/cab4d3e2f6a9059c059cdf1057bea46a2f7f9991b621356aeab5f4cc12731814/data?verify=1603150840-GfRxVS%2B80GZavwccPPv2em7zFGw%3D: proxyconnect tcp: dial tcp 192.168.65.1:3129: i/o timeout
 # 
 
 
