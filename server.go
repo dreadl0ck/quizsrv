c7f57000d147ed312e8e928781dccbce157634b6
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-10-31 15:07:01 +0100

    added anki import logic

--- server.go
+++ server.go
@@ -11,7 +11,9 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/dreadl0ck/cryptoutils"
+	"github.com/flimzy/anki"
 	"github.com/foomo/simplecert"
 	"github.com/foomo/tlsconfig"
 	"github.com/russross/blackfriday/v2"
@@ -24,6 +26,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -490,7 +493,7 @@ func connect(w http.ResponseWriter, r *http.Request) {
 			log.Println(r.RemoteAddr, "read:", err)
 			break
 		}
-		log.Println(r.RemoteAddr, "recv:", message)
+		log.Println(r.RemoteAddr, "recv:", string(message))
 
 		if len(done) == len(category) {
 			writeDone(c, r)
@@ -621,6 +624,103 @@ func main() {
 
 	flag.Parse()
 	rand.Seed(time.Now().UnixNano())
+
+	files, err := ioutil.ReadDir("courses/inr/Anki")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+
+		var q []*question
+
+		if f.IsDir() || !(filepath.Ext(f.Name()) == ".apkg") {
+			continue
+		}
+
+		apkg, err := anki.ReadFile(filepath.Join("courses/inr/Anki", f.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer apkg.Close()
+
+		os.MkdirAll("files/anki", 0700)
+		for _, fn := range apkg.ListFiles() {
+			data, err := apkg.ReadMediaFile(fn)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fi, err := os.Create(filepath.Join("files/anki", fn))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer fi.Close()
+
+			fi.Write(data)
+
+			fmt.Println("extracted", fn)
+		}
+
+		notes, err := apkg.Notes()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for {
+			notes.Next()
+			n, err := notes.Note()
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+
+			re := regexp.MustCompile("\\.([A-Z]+)")
+			r := strings.NewReplacer("\\n", "\n", "</div>", "", "<div>", "", "&gt;", ">", "&lt;", "<", ". ", ".\n", "<br />", "\n", "&nbsp;", " ", "<br>", "\n", "- ", "\n- ", "src=\"paste-", "src=\"/files/anki/paste-")
+			f := func(in string) string {
+
+				return r.Replace(
+					//strip.StripTags(
+						re.ReplaceAllStringFunc(in, func(m string) string{
+							return strings.Replace(m, ".", ".\n", 1)
+						}),
+					//),
+				)
+			}
+
+			if strings.HasPrefix(n.FieldValues[0], "What are the five different OSPF") {
+				spew.Dump(n)
+			}
+
+			q = append(q, &question{
+				Question: f(n.FieldValues[0]),
+				Answer:   f(n.FieldValues[1]),
+			})
+
+			if len(n.FieldValues) != 2 {
+				spew.Dump(n)
+			}
+		}
+
+		inrReplacer := strings.NewReplacer("INR-", "", "INR_", "", ".apkg", ".yml")
+		f, err := os.Create(inrReplacer.Replace("courses/inr/Anki/" +f.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data, err := yaml.Marshal(q)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		f.Write(data)
+
+		err = f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("done with", f.Name())
+	}
 
 	root := filepath.Join(*configFolder, "courses")
 	courses, err := ioutil.ReadDir(root)
